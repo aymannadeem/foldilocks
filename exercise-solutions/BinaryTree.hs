2d1b31c36795fa8cf9b1135cc381a6a47d120e01
Ayman Nadeem <aymannadeem@github.com>
2019-04-24 13:39:02 -0400

    Update BinaryTree.hs

--- exercise-solutions/BinaryTree.hs
+++ exercise-solutions/BinaryTree.hs
@@ -1,15 +1,18 @@
 module BinaryTree (foldB') where
 
+-- This is our Binary Tree:
 data B a = Empty | Leaf | B (Branch a) (Branch a)
 
+-- This is how we pattern match to recursively traverse our binary tree:
 foldB Empty = mempty
 foldB (Leaf a) = a
 foldB (Branch a b) = foldB a <> foldB b
 
--- foldMap!
+-- We can use foldMap to map each element to
+-- a monoid, and combine the results:
 foldBMap :: (Monoid b) => (a -> b) -> B a -> b
-foldBMap
 
+-- 
 foldB' :: (b -> b -> b) -> (a -> b) -> b -> B a -> b
 foldB' f g h (Branch a b) = foldB' f g h a `f` foldB' f g h a
 foldB' f g h (Leaf a) = g a
