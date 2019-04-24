8f3ac1cc752180b39d5d3cd46b538a57673de1b3
Ayman Nadeem <aymannadeem@github.com>
2019-04-24 13:12:11 -0400

    change ShowB to ShowFold

--- exercise-solutions/ShowFold.hs
+++ exercise-solutions/ShowFold.hs
@@ -1,10 +1,10 @@
-module ShowB (showB) where
+module ShowFold (showB) where
 
 foldB Empty = mempty
 foldB (Leaf a) = a
 foldB (Branch a b) = foldB a <> foldB b
 
 -- TODO: Write a function like showB, directly recursively
-showB :: (Show a) => B a -> String
--- go buckwild but visit entire tree
+showFold :: (Show a) => B a -> String
+-- go buckwild but visit entire tree,
 -- THEN define in terms of foldB'
