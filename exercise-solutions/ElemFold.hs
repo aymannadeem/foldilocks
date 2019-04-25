498e91dac354c089874e626fd975e8a58c60ec99
Bryan Enders <endersstocker@users.noreply.github.com>
2019-04-25 09:58:29 -0400

    Fix: Elem solution
    Why
    ---
    
    Product solution does not short circuit, unlike `Prelude.elem`
    
    E.g., `elem 42 $ repeat 42` evaluates `True`
    
    What
    ----
    
    * Use `Prelude.foldr` instead of `Prelude.foldl` to take advantage of
      short-circuiting
    * Use `Prelude.||` instead of `if-then-else` syntax
    * Eta reduce

--- exercise-solutions/ElemFold.hs
+++ exercise-solutions/ElemFold.hs
@@ -1,4 +1,4 @@
 module ElemFold (elem') where
 
 elem' :: (Eq a) => a -> [a] -> Bool
-elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+elem' y = foldr (\x acc -> x == y || acc) False
