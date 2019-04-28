13539218ba90281f0c940f327225017d812b5c34
Ayman Nadeem <aymannadeem@github.com>
2019-04-28 15:26:42 -0400

    update Head with manual and fold definitions, and add repl.it

--- exercise-solutions/Head.hs
+++ exercise-solutions/Head.hs
@@ -1,2 +1,13 @@
+module Head where
+
+-- Manual Recursion
 head' :: [a] -> a
-head' = foldr1 (\x _ -> x)
+head' [] = error "empty list! bye felicia"
+head' (x:xs) = x
+
+-- Using foldr1
+headFold :: [a] -> a
+headFold = foldr1 (\x _ -> x)
+
+-- Run in your browser:
+-- https://repl.it/@aymannadeem/Head
