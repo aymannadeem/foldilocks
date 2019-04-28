56a321c1f61a6ee6245f07ceb52ba6b12d6cfd0e
Ayman Nadeem <aymannadeem@github.com>
2019-04-28 15:40:05 -0400

    update last to use manual recursion and fold, add repl.it link

--- exercise-solutions/Last.hs
+++ exercise-solutions/Last.hs
@@ -1,2 +1,14 @@
+module Last where
+
+-- Manual Recursion
 last' :: [a] -> a
-last' = foldl1 (\_ x -> x)
+last' [x] = x
+last' (_:xs) = last' xs
+last' [] = error "this is an empty list! bye felicia"
+
+-- Using foldl1
+lastFold :: [a] -> a
+lastFold = foldl1 (\_ x -> x)
+
+-- Run in your browser:
+-- https://repl.it/@aymannadeem/Last
