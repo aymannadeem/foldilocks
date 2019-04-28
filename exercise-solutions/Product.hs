b50ba25a765791f855c47464d22d514de7361b18
Ayman Nadeem <aymannadeem@github.com>
2019-04-28 15:15:58 -0400

    update product to use multiple definitions, and add repl.it

--- exercise-solutions/Product.hs
+++ exercise-solutions/Product.hs
@@ -1,2 +1,13 @@
+module Product where
+
+-- Manual Recursion
 product' :: (Num a) => [a] -> a
-product' = foldr (*) 1
+product' [] = 1
+product' (x:xs) = x * product' xs
+
+-- Using foldr
+productFold :: (Num a) => [a] -> a
+productFold = foldr (*) 1
+
+-- Run in your browser:
+-- https://repl.it/@aymannadeem/productFold
