fa69b95f27969952764b3c18925c9f3466b9bbcc
Ayman Nadeem <aymannadeem@github.com>
2019-04-28 15:09:15 -0400

    replace lambda with flip (:)

--- exercise-solutions/Reverse.hs
+++ exercise-solutions/Reverse.hs
@@ -9,5 +9,9 @@ reverse' (x:xs) = reverse' xs ++ [x]
 reverseFold :: [a] -> [a]
 reverseFold = foldl (\acc x -> x : acc) []
 
+-- Avoid lambda by using flip (:) in place
+reverseFold' :: [a] -> [a]
+reverseFold' = foldl (flip (:)) []
+
 -- Run in your browser:
 -- https://repl.it/@aymannadeem/reverseFold
