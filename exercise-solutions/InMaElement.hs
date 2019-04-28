b1af1d1507025d74022d6b3c9d9d4ec1dcef8fff
Ayman Nadeem <aymannadeem@github.com>
2019-04-28 14:51:02 -0400

    export all variants of elem

---
+++ exercise-solutions/InMaElement.hs
@@ -0,0 +1,16 @@
+module InMaElement where
+
+-- Manual Recursion
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem'` xs
+
+-- Using foldl
+elemFold :: (Eq a) => a -> [a] -> Bool
+elemFold y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+-- Idiomatic, concise, eta-reduced using foldl:
+elemFold' :: (Eq a) => a -> [a] -> Bool
+elemFold' y = foldl (\acc x -> x == y || acc) False
