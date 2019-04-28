0cfaabc8eebda61be65b5f1988bc7255e410ad4c
Ayman Nadeem <aymannadeem@github.com>
2019-04-28 15:01:36 -0400

    express max with varying definitions

--- exercise-solutions/Max.hs
+++ exercise-solutions/Max.hs
@@ -1,2 +1,14 @@
-maximum' :: (Ord a) => [a] -> a
-maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+module Max where
+
+-- Manual Recursion
+maximum' :: Ord a => [a] -> a
+maximum' [] = error "bye felica"
+maximum' [x] = x
+maximum' (x:xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where maxTail = maximum' xs
+
+-- Using foldr1
+maxFold :: (Ord a) => [a] -> a
+maxFold = foldr1 (\x acc -> if x > acc then x else acc)
