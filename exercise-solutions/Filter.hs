0e5bbcd6d7f74e64471ef5bd0e6157ee6febac30
Ayman Nadeem <aymannadeem@github.com>
2019-04-28 15:23:17 -0400

    update filter to use multiple definitions and add repl.it link

--- exercise-solutions/Filter.hs
+++ exercise-solutions/Filter.hs
@@ -1,2 +1,15 @@
+module Filter where
+
+-- Manual Recursion
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+filter' p [] = []
+filter' p (x:xs)
+  | p x == True = [x]
+  | otherwise = filter' p xs
+
+-- Using foldr
+filterFold :: (a -> Bool) -> [a] -> [a]
+filterFold p = foldr (\x acc -> if p x then x : acc else acc) []
+
+-- Run in your browser:
+-- https://repl.it/@aymannadeem/filterFold
